e7de8d1c829dc20bfb3e4775822b21ea3250cf80
Tiago <tiagoufcg@gmail.com>
2024-02-15 09:27:41 -0300

    fix package.json #3

--- src/database.ts
+++ src/database.ts
@@ -2,6 +2,7 @@ import { knex as setupKnex, Knex } from 'knex'
 import { env } from './env'
 
 export const config: Knex.Config = {
+  client: env.DATABASE_CLIENT,
   connection:
     env.DATABASE_CLIENT == 'sqlite'
       ? {
